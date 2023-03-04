3ff9f7fd8c856dda302be1331d76a93fa1133b6f
Angel Tapia Ramírez <57466486+AngelTRam@users.noreply.github.com>
2023-03-04 12:43:17 -0700

    saving changes

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,2 +1,26 @@
 import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import RootClientTabs from './ClientTabs';
+import { Icon } from 'react-native-elements';
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator(){
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen
+                name='RootClientTabs'
+                component={RootClientTabs}
+                options={{
+                    title: 'Client',
+                    drawerIcon: ({focussed,size})=>(
+                        <Icon
+                        type='material-community'
+                        name='home'
+                        />
+                    )
+                }}
+            />
+        </Drawer.Navigator>
+    )
+}
